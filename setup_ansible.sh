38433020fe3856ba85039c10a040a69bd5955d45
Gridya-hub <61473089+Gridya-hub@users.noreply.github.com>
2025-08-05 11:25:35 +0300

    Update setup_ansible.sh

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt update && sudo apt upgrade -y
+
